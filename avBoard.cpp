ecf4a8fb0b339d1e8f0db90eb20e7afdbe94e784
philkkw <gol13002@byui.edu>
2019-05-30 21:35:03 -0600

    Update avBoard.cpp
    added header comment block

--- avBoard.cpp
+++ avBoard.cpp
@@ -1,2 +1,17 @@
+/*****************************************************
+ * FILE: avBoard.cpp
+ * 
+ * AUTHORS: Zac Carico
+ *          Parker Goldsberry
+ * 
+ * SUMMARY: Library for communicating with the avionics
+ *          board. This will provide a way for the
+ *          data from the board to be received.
+ * ***************************************************/
 #include "avBoard.h"
 
+
+void AvBoard::getAlt(void*data)
+{
+ 
+}
